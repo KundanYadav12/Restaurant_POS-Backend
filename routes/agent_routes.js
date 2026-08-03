353e6f8b6f7e12911d0c3b03bdf63695d378dcea
KundanYadav12 <kundanyadav96197@gmail.com>
2026-08-03 09:49:24 +0530

    we were faced not module no define issue not its resolved

--- routes/agent_routes.js
+++ routes/agent_routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const AgentController = require('../controllers/agent_controller');
-const { verifyToken } = require('../middlewares/auth');
+const { authenticateToken } = require('../middlewares/auth_middleware');
 
 // All agent routes require authenticated token (cashier, admin, or manager)
-router.use(verifyToken);
+router.use(authenticateToken);
 
 router.get('/print-jobs/poll', AgentController.getPendingJobs);
 router.post('/print-jobs/ack', AgentController.acknowledgeJob);
--- routes/sync_routes.js
+++ routes/sync_routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const SyncController = require('../controllers/sync_controller');
-const { verifyToken } = require('../middlewares/auth');
+const { authenticateToken } = require('../middlewares/auth_middleware');
 
-router.use(verifyToken);
+router.use(authenticateToken);
 
 router.post('/upward', SyncController.ingestUpwardSync);
 router.get('/downward', SyncController.serveDownwardSync);
